37ef22a9a72058af289f99964c81fff2028eebe0
chunyulo-code <chunyulo@arch.nctu.edu.tw>
2022-05-23 18:16:14 +0800

    add some functions and commands into the tranin_data like 'fly, switch view, start bpc, pause and continue fight...'

--- scriptInUe4.py
+++ scriptInUe4.py
@@ -1,8 +1,8 @@
 import json
 f=open(r"D:\sttNlp\output.txt","r")
 data_dict = json.load(f)
-dir = data_dict['dir']
-bui = data_dict['bui']
-func = data_dict['func'] 
+dir = data_dict['dir'].lower()
+bui = data_dict['bui'].lower()
+func = data_dict['func'] .lower()
 
 print(dir, bui, func)
